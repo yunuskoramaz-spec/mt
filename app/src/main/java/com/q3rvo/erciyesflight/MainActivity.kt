e4fd1211da2ffc438e93980f0d23693bd3bf08d0
yunuskoramaz-spec <yunuskoramaz@gmail.com>
2026-09-13 00:46:06 +0300

    Add Android game activity

---
+++ app/src/main/java/com/q3rvo/erciyesflight/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.q3rvo.erciyesflight
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(GameView(this))
+    }
+}
